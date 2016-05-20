0bb4e6c526386c82deff4ab6781615cf3559de1e
Colin Kierans <kierans@monstercat.com>
2016-05-20 14:05:24 -0700

    Updating playlist order with text boxes
    Summary:
    Updating playlist order with text boxes
    Added nodemon watch script
    
    I did a pull origin master
    
    Playlist tracks text field. Just logs values for now.
    
    Merged in master
    
    Saving playlist order to server and calling rerender of playlist.
    
    Test Plan: null
    
    Reviewers: tmathews
    
    Reviewed By: tmathews
    
    Differential Revision: https://phabricator.monstercat.com/D241

--- public/main.js
+++ public/main.js
@@ -10,6 +10,7 @@ var strings   = {
   "accountUpdated": "Your account information has been saved.",
   "settingsUpdated": "Your preferred settings have been updated.",
   "addedToPlaylist": "Song succesfully added to playlist.",
+  "reorderedPlaylist": "Playlist order saved.",
   "removedFromPlaylist": "Song succesfully removed from playlist.",
   "passwordMissing": "You must enter a password.",
   "passwordReset": "Your password has been reset. Please sign in.",
@@ -171,7 +172,7 @@ function destroyPlaylist (e, el) {
 function removeFromPlaylist (e, el) {
   var pel   = document.querySelector('[playlist-id]')
   var id    = pel ? pel.getAttribute('playlist-id') : ""
-  var url   = endpoint + '/playlist/' + id + '?fields=name,public,tracks'
+  var url   = endpoint + '/playlist/' + id + '?fields=name,public,tracks,userId'
   var index = parseInt(el.getAttribute('playlist-position'))
   if (!id) return window.alert(strings.error)
   loadCache(url, function (err, obj) {
@@ -394,7 +395,7 @@ function updatePlayerPlaylist (playlistId, ptracks) {
   loadCache(url, function(err, obj) {
     if (err) return window.alert(err) // TODO handle this error better
     var tracks = obj.results.map(function (item, index) {
-      var track = mapReleaseTrack(item, ptracks[index].releaseId, index)
+      var track = mapReleaseTrack(item, index)
       track.playlistId = playlistId
       return track
     })
@@ -724,19 +725,6 @@ function closeModal () {
   container.removeChild(container.firstElementChild)
 }
 
-function renderHeader () {
-  var el = document.querySelector('#navigation')
-  var target = '[template-name="' + el.getAttribute('template') + '"]'
-  var template = document.querySelector(target).textContent
-  var data = null
-  if (session) {
-    data = {}
-    data.user = session ? session.user : null
-  }
-  render(el, template, {
-    data: data
-  })
-}
 
 function togglePassword (e, el) {
   var target = 'input[name="' + el.getAttribute('toggle-target') + '"]'
@@ -765,8 +753,192 @@ function simpleUpdate (err, obj, xhr) {
   loadSubSources(document.querySelector('[role="content"]'), true, true)
 }
 
+function reorderPlaylistFromInputs() {
+  var inputs = document.querySelectorAll('[name="trackOrder\\[\\]"')
+  //This is a kinda hacky way for not letting them accidentally delete all their tracks
+  //by spam clicking while the track list is reloading
+  if(inputs.length == 0) {
+    return
+  }
+  var trackOrdering = []
+  var trackEls = []
+  var changed = false
+  for(var i = 0; i < inputs.length; i++) {
+    var input = inputs[i]
+    var trackId = input.getAttribute('track-id')
+    var releaseId = input.getAttribute('release-id')
+    var to = parseInt(input.value)
+    var from = i + 1
+    if(!changed) {
+      changed = to != from
+    }
+    trackOrdering.push({trackId: trackId, releaseId: releaseId, from: from, to: to})
+  }
+  if(!changed) return
+  trackOrdering.sort(function(a, b) {
+    //If you change #1 to #6 and leave #6 at #6 then track 1 should be after #6
+    //If you move #7 to #3 and leave #3 unchanged, then #7 should be before #3
+    if(a.to == b.to) {
+      if(a.to > a.from) {
+        return 1
+      }
+      if(a.to < a.from) {
+        return -1
+      }
+      if(b.to > b.from) {
+        return -1
+      }
+      if(b.to < b.from) {
+        return 1
+      }
+      return 0
+    }
+    return a.to > b.to ? 1 : -1
+  })
+  trackEls = trackOrdering.map(function (item) {
+    return document.querySelector('tr[role="playlist-track"][track-id="' + item.trackId + '"][release-id="' + item.releaseId + '"]')
+  })
+  var tracksNode = document.querySelector('[role="playlist-tracks"]')
+  for(var i = trackEls.length - 1; i >= 0; i--) {
+    var before = i == (trackEls.length - 1) ? null : trackEls[i+1]
+    tracksNode.insertBefore(tracksNode.removeChild(trackEls[i]), before)
+  }
+  resetPlaylistInputs()
+  savePlaylistOrder();
+}
+
+function resetPlaylistInputs() {
+  var trackEls = document.querySelectorAll('[role="playlist-track"]')
+  for(var i = 0; i < trackEls.length; i++) {
+    trackEls[i].querySelector('input[name="trackOrder\[\]"]').value = (i + 1)
+  }
+}
+
+function savePlaylistOrder() {
+  var id = document.querySelector('[playlist-id]').getAttribute('playlist-id')
+  var trackEls = document.querySelectorAll('[role="playlist-track"]')
+  var trackSaves = []
+  for(var i = 0; i < trackEls.length; i++) {
+    trackSaves.push({
+      trackId: trackEls[i].getAttribute('track-id'),
+      releaseId: trackEls[i].getAttribute('release-id')
+    })
+  }
+  var url   = endpoint + '/playlist/' + id + '?fields=name,public,tracks,userId'
+  update('playlist', id, {tracks: trackSaves}, function (err, obj, xhr) {
+    if (err) {
+      toast({
+        error: true,
+        message: err.message
+      })
+      return
+    }
+    cache(url, obj)
+    simpleUpdate()
+    toast({
+      message: strings.reorderedPlaylist
+    })
+  })
+}
+
+function renderHeader () {
+  var el = document.querySelector('#navigation')
+  var target = '[template-name="' + el.getAttribute('template') + '"]'
+  var template = document.querySelector(target).textContent
+  var data = null
+  if (session) {
+    data = {}
+    data.user = session ? session.user : null
+  }
+  render(el, template, {
+    data: data
+  })
+}
+
 function canAccessGold (e, el) {
   if (hasGoldAccess()) return
   e.preventDefault()
   openModal('subscription-required-modal')
+}
+
+function playlistTrackOrderFocus(e, el) {
+  el.closest('[role="playlist-track"]').setAttribute('draggable', 'false')
+}
+
+function playlistTrackOrderBlur(e, el) {
+  el.closest('[role="playlist-track"]').setAttribute('draggable', 'true')
+}
+
+function playlistDragStart (e, trackId, releaseId) {
+  e.dataTransfer.setData("trackId", trackId)
+  e.dataTransfer.setData("releaseId", releaseId)
+  e.dataTransfer.setData("childIndex", getChildIndex(e.target))
+  e.target.closest('[role="playlist-track"]').classList.add('drag-dragging')
+}
+
+function getOffset( el ) {
+  var _x = 0;
+  var _y = 0;
+  while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+    _x += el.offsetLeft - el.scrollLeft;
+    _y += el.offsetTop - el.scrollTop;
+    el = el.offsetParent;
+  }
+  return { top: _y, left: _x };
+}
+
+function getEventVertHalf (e, el) {
+  var offset = getOffset(el)
+  var height = el.offsetHeight
+  if(e.clientY < (offset.top + (height / 2))) {
+    return 'top'
+  }
+  return 'bottom'
+}
+
+function playlistAllowDrop (e) {
+  e.preventDefault();
+  var targetTr = e.target.closest('[role="playlist-track"]')
+  targetTr.classList.add('drag-active')
+  var half = getEventVertHalf(e, targetTr)
+  if(half == 'top') {
+    targetTr.classList.add('drag-active-top')
+    targetTr.classList.remove('drag-active-bottom')
+  }
+  else {
+    targetTr.classList.add('drag-active-bottom')
+    targetTr.classList.remove('drag-active-top')
+  }
+}
+
+function playlistDragLeave (e) {
+  e.target.closest('[role="playlist-track"]').classList.remove('drag-active', 'drag-active-top', 'drag-active-bottom')
+}
+
+function getChildIndex (child){
+  var parent = child.parentNode;
+  var children = parent.children;
+  var i = children.length - 1;
+  for (; i >= 0; i--){
+    if (child == children[i]){
+      return i
+    }
+  }
+  return i
+}
+
+function playlistDrop (e) {
+  var trackId = e.dataTransfer.getData('trackId')
+  var releaseId = e.dataTransfer.getData('releaseId')
+  var droppedTr = e.target.closest('[role="playlist-track"]')
+  var draggedTr = document.querySelector('tr[role="playlist-track"][track-id="' + trackId + '"][release-id="' + releaseId + '"]')
+  draggedTr.classList.remove('drag-dragging')
+  var draggedIndex = e.dataTransfer.getData('childIndex')
+  var droppedIndex = getChildIndex(droppedTr)
+  var half = getEventVertHalf(e, droppedTr)
+  var insertBefore = half == 'top' ? droppedTr : droppedTr.nextSibling
+  droppedTr.parentNode.insertBefore(draggedTr, insertBefore)
+  droppedTr.classList.remove('drag-active', 'drag-active-bottom', 'drag-active-top')
+  resetPlaylistInputs()
+  savePlaylistOrder()
 }
