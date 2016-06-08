2eef383cef25e4495f1ae1985dae026a2521d3b4
tmathews <thomas.c.mathews@gmail.com>
2016-06-08 13:18:40 -0700

    Replaced ES6 player class with old school function.

--- public/player.js
+++ public/player.js
@@ -1,192 +1,202 @@
-class MusicPlayer {
-  constructor() {
-    constructEmitter(this)
+function MusicPlayer () {
+  constructEmitter(this)
+  MusicPlayer.define(this)
+  this.audio = new Audio()
+  this.audio.addEventListener('error', onError.bind(this))
+  this.audio.addEventListener('stalled', onStalled.bind(this))
+  this.audio.addEventListener('ended', onEnded.bind(this))
+  this.audio.addEventListener('timeupdate', onTimeUpdate.bind(this))
+  this.audio.addEventListener('loadedmetadata', onStateChange.bind(this))
+  this.audio.addEventListener('loadeddata', onStateChange.bind(this))
+  this.audio.addEventListener('loadstart', onStateChange.bind(this))
+  this.audio.addEventListener('canplay', onStateChange.bind(this))
+  this.audio.addEventListener('canplaythrough', onStateChange.bind(this))
+  this.audio.addEventListener('progress', onStateChange.bind(this))
+  this.audio.addEventListener('pause', onStateChange.bind(this))
+  this.repeatMode = 'none'
+  this.shuffle = false
+  this.clear()
+}
 
-    this.audio = new Audio()
-    this.audio.addEventListener('error', onError.bind(this))
-    this.audio.addEventListener('stalled', onStalled.bind(this))
-    this.audio.addEventListener('ended', onEnded.bind(this))
-    this.audio.addEventListener('timeupdate', onTimeUpdate.bind(this))
+MusicPlayer.prototype.add = function (item) {
+  if (!item || !item.source)
+    return -1
 
-    this.audio.addEventListener('loadedmetadata', onStateChange.bind(this))
-    this.audio.addEventListener('loadeddata', onStateChange.bind(this))
-    this.audio.addEventListener('loadstart', onStateChange.bind(this))
-    this.audio.addEventListener('canplay', onStateChange.bind(this))
-    this.audio.addEventListener('canplaythrough', onStateChange.bind(this))
-    this.audio.addEventListener('progress', onStateChange.bind(this))
-    this.audio.addEventListener('pause', onStateChange.bind(this))
+  var duplicate = this.items.findIndex(function(aitem) { return aitem.source == item.source })
+  if (duplicate > -1)
+    return duplicate
 
-    this.repeatMode = 'none'
-    this.shuffle = false
+  this.items.push(item)
+  return this.items.length - 1
+}
 
-    this.clear()
-  }
+MusicPlayer.prototype.removeAt = function (index) {
+  if (index >= 0 && index < this.items.length)
+    this.items.splice(index, 1)
+}
 
-  add(item) {
-    if (!item || !item.source)
-      return -1
-
-    var duplicate = this.items.findIndex(function(aitem) { return aitem.source == item.source })
-    if (duplicate > -1)
-      return duplicate
-
-    this.items.push(item)
-    return this.items.length - 1
-  }
-
-  removeAt(index) {
-    if (index >= 0 && index < this.items.length)
-      this.items.splice(index, 1)
-  }
-
-  addAndPlay(item) {
-    var index = this.add(item)
-    if (index > -1)
-      this.play(index)
-  }
-
-  // If index is undefined, it will start first or resume currently playing song.
-  play(index) {
-    if (!this.items || !this.items.length || ((index != null) && (!this.items[index] || !this.items[index].source)))
-      return
-
-    index = index == undefined ? +this.index : +index
-    this.index = index
-    if (this.audio.src != this.items[index].source) {
-      this.audio.src = this.items[index].source
-    }
-    this.audio.autoplay = true
-    this.audio.play()
-    this.index = index
-
-    this.dispatchEvent(new CustomEvent('play', {detail: {item: this.items[this.index]}}))
-  }
-
-  clear() {
-    this.index = 0
-    this.played = []
-    this.items = []
-  }
-
-  set(items) {
-    this.clear()
-    this.items = items
-  }
-
-  restart() {
-    this.seek(0)
-    if (!this.playing) this.play()
-  }
-
-  pause() {
-    this.audio.pause()
-  }
-
-  stop() {
-    this.pause()
-    this.seek(0)
-  }
-
-  toggle(index) {
-    if (index == undefined || index == this.index) {
-      return this.audio.paused ? this.play() : this.pause()
-    }
+MusicPlayer.prototype.addAndPlay = function (item) {
+  var index = this.add(item)
+  if (index > -1)
     this.play(index)
+}
+
+// If index is undefined, it will start first or resume currently playing song.
+MusicPlayer.prototype.play = function (index) {
+  if (!this.items || !this.items.length || ((index != null) && (!this.items[index] || !this.items[index].source)))
+    return
+
+  index = index == undefined ? +this.index : +index
+  this.index = index
+  if (this.audio.src != this.items[index].source) {
+    this.audio.src = this.items[index].source
   }
+  this.audio.autoplay = true
+  this.audio.play()
+  this.index = index
 
-  seek(percent) {
-    if (this.audio.error || !this.seekable)
-      return
+  this.dispatchEvent(new CustomEvent('play', {detail: {item: this.items[this.index]}}))
+}
 
-    percent = clamp(percent, 0, 1)
-    this.audio.currentTime = percent * this.audio.duration
+MusicPlayer.prototype.clear = function () {
+  this.index = 0
+  this.played = []
+  this.items = []
+}
+
+MusicPlayer.prototype.set = function (items) {
+  this.clear()
+  this.items = items
+}
+
+MusicPlayer.prototype.restart = function () {
+  this.seek(0)
+  if (!this.playing) this.play()
+}
+
+MusicPlayer.prototype.pause = function () {
+  this.audio.pause()
+}
+
+MusicPlayer.prototype.stop = function () {
+  this.pause()
+  this.seek(0)
+}
+
+MusicPlayer.prototype.toggle = function (index) {
+  if (index == undefined || index == this.index) {
+    return this.audio.paused ? this.play() : this.pause()
   }
+  this.play(index)
+}
 
-  setVolume(percent) {
-    percent = clamp(percent, 0, 1)
-    this.audio.volume = percent
-  }
+MusicPlayer.prototype.seek = function (percent) {
+  if (this.audio.error || !this.seekable)
+    return
 
-  next() {
-    this.advance(1)
-  }
+  percent = clamp(percent, 0, 1)
+  this.audio.currentTime = percent * this.audio.duration
+}
 
-  previous() {
-    this.advance(-1)
-  }
+MusicPlayer.prototype.setVolume = function (percent) {
+  percent = clamp(percent, 0, 1)
+  this.audio.volume = percent
+}
 
-  advance(amount) {
-    var items = this.items
-    if (!items || !items.length)
-      return
+MusicPlayer.prototype.next = function () {
+  this.advance(1)
+}
 
-    if (this.shuffle) {
-      // Handle case where already played song and now shuffling
-      if (this.playable && !this.played.length) {
-        this.played.push(this.index)
-      }
+MusicPlayer.prototype.previous = function () {
+  this.advance(-1)
+}
 
-      var playedIndex = this.played.length - 1 + amount
-        , exhausted = playedIndex >= items.length || playedIndex < 0
+MusicPlayer.prototype.advance = function (amount) {
+  var items = this.items
+  if (!items || !items.length)
+    return
 
-      if (exhausted) {
-        this.played = []
-      }
-
-      if (playedIndex >= 0 && playedIndex < this.played.length) {
-        this.index = playedIndex
-      }
-      else {
-        while (this.played.indexOf(this.index) > -1) {
-          this.index = Math.floor(Math.random() * items.length)
-        }
-      }
-
+  if (this.shuffle) {
+    // Handle case where already played song and now shuffling
+    if (this.playable && !this.played.length) {
       this.played.push(this.index)
     }
-    else {
+
+    var playedIndex = this.played.length - 1 + amount
+      , exhausted = playedIndex >= items.length || playedIndex < 0
+
+    if (exhausted) {
       this.played = []
-      this.index = (this.index + amount) % items.length
-      if (this.index < 0)
-        this.index += this.items.length
     }
 
-    this.play(this.index)
+    if (playedIndex >= 0 && playedIndex < this.played.length) {
+      this.index = playedIndex
+    }
+    else {
+      while (this.played.indexOf(this.index) > -1) {
+        this.index = Math.floor(Math.random() * items.length)
+      }
+    }
+
+    this.played.push(this.index)
+  }
+  else {
+    this.played = []
+    this.index = (this.index + amount) % items.length
+    if (this.index < 0)
+      this.index += this.items.length
   }
 
-  get playable() {
-    return this.audio.readyState >= 3
-  }
+  this.play(this.index)
+}
 
-  get playing() {
-    return this.playable && !this.audio.paused
-  }
+MusicPlayer.define = function (obj) {
+  Object.defineProperty(obj, 'playable', {
+    get: function () {
+      return this.audio.readyState >= 3
+    }
+  })
 
-  get loading() {
-    return (this.audio.readyState >= 1 && this.audio.readyState <= 2) || this.audio.networkState == 2
-  }
+  Object.defineProperty(obj, 'playing', {
+    get: function () {
+      return this.playable && !this.audio.paused
+    }
+  })
 
-  get seekable() {
-    return this.audio.readyState >= 1 && this.audio.seekable
-  }
+  Object.defineProperty(obj, 'loading', {
+    get: function () {
+      return (this.audio.readyState >= 1 && this.audio.readyState <= 2) || this.audio.networkState == 2
+    }
+  })
 
-  get progress() {
-    return this.audio.duration ? this.audio.currentTime / this.audio.duration : 0
-  }
+  Object.defineProperty(obj, 'seekable', {
+    get: function () {
+      return this.audio.readyState >= 1 && this.audio.seekable
+    }
+  })
 
-  get finished() {
-    return this.repeat == 'none' && ((!this.shuffle && this.index >= this.items.length - 1) ||
-            this.played.length >= this.items.length - 1)
-  }
+  Object.defineProperty(obj, 'progress', {
+    get: function () {
+      return this.audio.duration ? this.audio.currentTime / this.audio.duration : 0
+    }
+  })
 
-  get repeat() {
-    return this.repeatMode
-  }
+  Object.defineProperty(obj, 'finished', {
+    get: function () {
+      return this.repeat == 'none' && ((!this.shuffle && this.index >= this.items.length - 1) ||
+                                      this.played.length >= this.items.length - 1)
+    }
+  })
 
-  set repeat(value) {
-    this.repeatMode = value
-    this.audio.loop = value == 'one'
-  }
+  Object.defineProperty(obj, 'repeat', {
+    get: function () {
+      return this.repeatMode
+    },
+    set: function (value) {
+      this.repeatMode = value
+      this.audio.loop = value == 'one'
+    }
+  })
 }
 
 function onError(e) {
