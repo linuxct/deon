924e7e9d1afefb83d9e9b63765d49b42626e5a02
jimmy <jimmy@monstercat.com>
2016-06-27 13:10:39 -0700

    Format subscription end date

--- public/main.js
+++ public/main.js
@@ -491,7 +491,7 @@ function transformWhitelists (obj) {
     whitelist.paid = (whitelist.amountPaid / 100).toFixed(2)
     whitelist.remaining = (whitelist.amountRemaining / 100).toFixed(2)
     if (whitelist.availableUntil)
-      whitelist.nextBillingDate = whitelist.availableUntil
+      whitelist.nextBillingDate = formatDate(whitelist.availableUntil)
     if (whitelist.subscriptionActive)
       whitelist.cost = (5).toFixed(2)
     whitelist.monthlyCost = 500
