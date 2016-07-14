726a2d20417bdb87f47c19c154261521fad3ba54
tmathews <thomas.c.mathews@gmail.com>
2016-07-14 14:50:13 -0700

    Changed dev server to read files on request.

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 var express 	= require('express')
 var path    	= require('path')
-var fs 			= require('fs')
+var fs 			  = require('fs')
+
 var dir = process.env.PUBLIC_DIR || process.cwd() + '/public'
 if (!path.isAbsolute(dir)) dir = path.resolve(process.cwd(), dir)
-var indexPath	= path.resolve(process.cwd(), 'cache', 'index.html')
-var templatesDir = path.resolve(dir, 'templates')
-var files = fs.readdirSync(templatesDir)
-var templatesHTML = ''
-files.forEach(function (file) {
-	if (file.substr(0, 1) != '_') {
-		templatesHTML += "<!-- START:" + file + " -->\n" 
-			+ fs.readFileSync(path.resolve(templatesDir, file), 'utf8')
-			+ "<!-- END:" + file + " -->\n" 
-	}
-})
-var html = fs.readFileSync(path.resolve(templatesDir, '_header.html')) 
-	+ templatesHTML 
-	+ fs.readFileSync(path.resolve(templatesDir, '_footer.html'))
-fs.writeFile(indexPath, html)
+
+var order = [
+  'begin',
+  'head',
+  'begin-body',
+  'body'
+]
+function buildHTML () {
+  var str = ""
+  var tdir = path.join(dir, 'templates')
+  var hdir = path.join(dir, 'html')
+  var arr = order.map(function (i) {
+    return path.join(hdir, i + '.html')
+  })
+  arr = arr.concat(fs.readdirSync(tdir).map(function (i) {
+    return path.join(tdir, i)
+  }))
+  arr.push(path.join(hdir, 'end.html'))
+  arr.forEach(function (i) {
+    str += fs.readFileSync(i, 'utf8')
+  })
+  return str
+}
+
 var app = express()
 app.use(express.static(dir))
 app.use((req, res, next)=>{
-  res.sendFile(indexPath)
+  res.status(200).send(buildHTML())
 })
+
 app.listen(process.env.PORT || 8080)
