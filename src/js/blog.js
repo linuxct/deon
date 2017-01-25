30921d1137f022118ec21981e853676a9dea6784
Juan Ferreras <juan@flioh.com>
2017-01-25 17:46:38 -0300

    Bug fixing (#148)
    * fixed event sponsorship bg
    
    * fix select height on tables and right alignment on browse table
    
    * fixed blog excerpt excessive length and stripped down html content
    
    * fixed reddit embed not loading on new blog
    
    * added styles for h4, h5, h6 globally
    
    * replaced spotify playlists on homepage
    
    * compressed down spotify images

--- src/js/blog.js
+++ src/js/blog.js
@@ -4,6 +4,14 @@ function transformPost(obj){
 	obj.image = transformLegacyImages(obj.image)
 	return obj
 }
+function completedMarkdownPost(){
+  var redditEmbeds = document.querySelector('.reddit-embed');
+  if (!redditEmbeds) return
+
+  var redditJs = document.createElement('script');
+  redditJs.src = 'https://www.redditstatic.com/comment-embed.js';
+  document.getElementsByTagName('head')[0].appendChild(redditJs);
+}
 function transformBlogPagination(obj){
   var q = queryStringToObject(window.location.search)
   q.page = parseInt(q.page) || 1
@@ -14,14 +22,23 @@ function transformBlog(obj){
   if (obj.total > 1) obj.showPagination = true
   setPagination(obj, obj.limit)
 
+  var maxExcerpt = 200
+
   obj.results.forEach(function (i, index, arr) {
   	i.featured = (index == 0) ? true : false
     i.date = formatDate(i.date)
     i.isOdd = !(index % 2 == 0)
+    i.excerpt = transformExcerptToText(i.excerpt)
+    i.excerpt = (i.excerpt.length > maxExcerpt) ? i.excerpt.substr(0,maxExcerpt)+'...' : i.excerpt
     i.image = transformLegacyImages(i.image)
     i.url = i.path.split('/')[1].slice(0, -3) // remove 'posts/' and '.md'
   })
   return obj
+}
+function transformExcerptToText(htmlExcerpt){
+  var aux = document.createElement('div')
+  aux.innerHTML = htmlExcerpt
+  return aux.textContent || aux.innerText || "";
 }
 function transformLegacyImages(img){
 	return (img.indexOf('http') == -1) ? img = 'https://www.monstercat.com' + img : img
