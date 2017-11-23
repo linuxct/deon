f40d07e8b158944a0ed93a339ae758b081fa5bc2
Colin Kierans <vindexus@gmail.com>
2017-11-23 13:24:48 -0800

    Black friday (#286)
    * Black friday header with countdowns.
    
    * Black friday banner.
    
    * Tweaks to black friday.
    
    * Fix merge stuff.

--- src/js/blackfriday.js
+++ src/js/blackfriday.js
@@ -2,8 +2,6 @@ var blackFridayStart = new Date("Thu Nov 23 2017 21:00:00 GMT-0800 (PST)");
 var blackFridayEnd = new Date("Mon Nov 27 2017 21:00:00 GMT-0800 (PST)");
 var blackFridayCountdownEnded = false;
 
-//blackFridayStart = new Date(new Date().getTime() + (5 * 1000));
-
 document.addEventListener('DOMContentLoaded', function() {
   renderBlackFriday();
 });
@@ -26,21 +24,18 @@ function renderBlackFriday () {
   }
   var countdown = document.querySelector('#black-friday [role=countdown]');
   countdown.setAttribute('to', to.toString());
+  countdown.removeAttribute('completed-called');
   document.querySelector('#black-friday .label').innerHTML = label;
   startCountdownTicks(); //Always on for black friday
 }
 
 function hideBlackFriday () {
-  document.getElementById('black-friday').styles.display = 'none';
+  document.getElementById('black-friday').style.display = 'none';
   document.getElementById('black-friday').innerHTML = '';
 }
 
 function blackFridayCountdownEnd () {
-  if(blackFridayCountdownEnded) {
-    return
-  }
   setTimeout(function () {
     renderBlackFriday();
-    blackFridayCountdownEnded = true;
   }, 1000);
 }
